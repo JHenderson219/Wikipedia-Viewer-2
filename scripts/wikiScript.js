aa5d8b102e572d49cd4a4878e0c43cd3c51ab3dc
JHenderson219 <hendersonj219@gmail.com>
2016-10-22 16:59:03 -0700

    JSON GET works

--- scripts/wikiScript.js
+++ scripts/wikiScript.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
-	$.ajax({
+	var userSearch= "test";
+	/*$.ajax({
 		url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions"+
 		"&rvprop=content&format=jsonfm", 
 		type: "GET",
-		dataType: "json",
+		dataType: "jsonp",
 		headers: {
-			"Api-User-Agent": "Wikipedia-viewer"
+			"API-User-Agent": "Wikipedia-Viewer/0.1 (codepen.io/https://codepen.io/JHenderson219/pen/VKgLpy, hendersonj219@gmail.com)",
+			"Access-Control-Allow-Origin":"*",
+			"Access-Control-Allow-Origin": "True"
 		},
 		success: function(json1){
 		$("#jsonTest").html(json1)},
 		error: alert("Failed to get API Data!")
+	});*/
+	var jsonData = $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search"
+		+"&origin=*&gsrsearch="+userSearch, function(data){
+	});
+	console.log(jsonData.length);
+	$("#search").on("click", function(){
+		$("#jsonTest").html("<h3 class = 'text-center'>"+jsonData+"</h3>");
 	});
 });
