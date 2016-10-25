c743d2a83e4613d73e78ff638ca049348e7dbd5d
JHenderson219 <hendersonj219@gmail.com>
2016-10-24 17:09:06 -0700

    Functional Build
    User Stories are fulfilled and build is functional. Lots of improvements
    can still be made, mostly UI/UX and other such improvements. Need to
    check mobile responsiveness. Will likely come back after learning more
    design and react stuff.

--- scripts/wikiScript.js
+++ scripts/wikiScript.js
@@ -1,23 +1,43 @@
 $(document).ready(function(){
-	var userSearch = "programming%20language";
-	var keys;
-	var searchIndices=[];
-	var titleArray = [];
-	var data = $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&"+
+	$("#search").on("tap", function(){
+		var userSearch = document.getElementById("queryBox")[0].value;
+		var keys;
+		var searchIndices=[];
+		var titleArray = [];
+		var titleStr;
+		var data;
+		$(".queryBox").html('<form id="queryBox"> <input type="text" name="search Wikipedia" placeholder="Search Wikipedia for..." class="center-block animated fadeIn"></form>');
+		$("#searchResults").empty();
+		data = $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&"+
 		"generator=search&origin=*&gsrsearch="+userSearch, function(json){
-		data = json["query"]["pages"];
-		keys = Object.keys(data);
-		console.log(keys); //keys is an array of the user's search keys
-		keys.forEach(function(key){
-			titleArray.push(data[key]["title"]);
-		});
-		console.log(titleArray);
+			data = json["query"]["pages"];
+			keys = Object.keys(data);
+			keys.forEach(function(key){ //creates array of titles and HTML formatting
+			titleArray.push("<a class= 'animated fadeIn' target = '_blank' href = 'https://en.wikipedia.org/?curid="+key+"'>"+data[key]["title"]+"</a>");
+			});
+		titleStr = titleArray.join('<br>');
+		$("#searchResults").html("<h3 class = 'text-center animated fadeIn'>"+titleStr+"</h3>");
+		});	
 	});
 	
 	$("#search").on("click", function(){
-		var titleStr = titleArray.join('<br>');
-		console.log(titleStr);
-		$("#jsonTest").html("<p class = 'text-center'>"+titleStr+"</p>");
-
+		var userSearch = document.getElementById("queryBox")[0].value;
+		var keys;
+		var searchIndices=[];
+		var titleArray = [];
+		var titleStr;
+		var data;
+		$(".queryBox").html('<form id="queryBox"> <input type="text" name="search Wikipedia" placeholder="Search Wikipedia for..." class="center-block animated fadeIn"></form>');
+		$("#searchResults").empty();
+		data = $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&"+
+		"generator=search&origin=*&gsrsearch="+userSearch, function(json){
+			data = json["query"]["pages"];
+			keys = Object.keys(data);
+			keys.forEach(function(key){ //creates array of titles and HTML formatting
+			titleArray.push("<a class= 'animated fadeIn' target = '_blank' href = 'https://en.wikipedia.org/?curid="+key+"'>"+data[key]["title"]+"</a>");
+			});
+		titleStr = titleArray.join('<br>');
+		$("#searchResults").html("<h3 class = 'text-center animated fadeIn'>"+titleStr+"</h3>");
+		});	
 	});
 });
